7b64c016146693ee9822a4ed4db7207800e643ee
unknown <Brainnovation@Janak-PC.braindigit.local>
2020-02-13 17:51:54 +0545

    db connection through api

---
+++ Helper/Data/SchoolContext.cs
@@ -0,0 +1,21 @@
+﻿using Helper.Model;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Helper.Data
+{
+    public class SchoolContext:DbContext
+    {
+        public SchoolContext(DbContextOptions<SchoolContext>options):base(options)
+        {
+
+
+        }
+        public DbSet<Course> Courses { get; set; }
+        public DbSet<Enrollment> Enrollments { get; set; }
+        public DbSet<Student> Students { get; set; }
+
+    }
+}
---
+++ Helper/Model/Course.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Helper.Model
+{
+    public class Course
+    {
+        [DatabaseGenerated(DatabaseGeneratedOption.None)]
+        [Key]
+        public int CourseID { get; set; }
+        public string Title { get; set; }
+        public int Credits { get; set; }
+        public ICollection<Enrollment> Enrollments { get; set; }
+    }
+}
